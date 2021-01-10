114c6d91c611474d7a91974438515c07d3d66a29
mlodovico <murilo.lodovico@outlook.com>
2021-01-10 19:19:58 -0300

    01.00.04 route for acess de database

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,10 @@
 import express from 'express';
 
+import routes from './routes';
+
 const app = express();
 
 app.use(express.json());
-
-app.post('/', (req, res) => {
-    return res.send('Hello World')
-})
+app.use(routes)
 
 app.listen(3333);
