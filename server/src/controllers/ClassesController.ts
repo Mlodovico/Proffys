32f3cfa55faafe04086bd8bff99aab809f208ea9
mlodovico <murilo.lodovico@outlook.com>
2021-01-14 22:01:38 -0300

    01.00.07 filter list classes

--- server/src/controllers/ClassesController.ts
+++ server/src/controllers/ClassesController.ts
@@ -26,8 +26,16 @@ export default class ClassesController {
         const timeInMinutes = convertHoursToMinutes(time);
 
         const classes = await db('classes')
+            .whereExists(function() {
+                this.select('class_schedule.*')
+                .from('class_schedule')
+                .whereRaw('`class_schedule`.`class_id` = `classes`.`id`')
+                .whereRaw('`class_schedule`.`week_day` = ??', [Number(week_day)])
+                .whereRaw('`class_schedule`.`from` <= ??', [timeInMinutes])
+                .whereRaw('`class_schedule`.`to` > ??', [timeInMinutes])
+            })
             .where('classes.subject', '=', subject)
-            .join('users', '=' ,'classes.user_id')
+            .join('users', 'classes.user_id', '=', 'users.id')
             .select(['classes.*', 'users.*'])
 
         return response.status(200).send(classes);
